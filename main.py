49b3da512db2db153c6d85e85449b6d3d19685e6
Brandon Dickson <brandondickson@Brandons-Mac-mini.local>
2025-03-01 13:07:37 +0000

    initial websocket setup

--- main.py
+++ main.py
@@ -1,8 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 
 app = FastAPI()
 
 
-@app.get("/")
-def read_route():
-    return {"Hello": "World"}
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections = list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send_personal_message(sale, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect()
+    try:
+        while True:
+            await websocket.send_text("Connected")
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.broadcast("Disconnected")
