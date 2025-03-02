386c1fd06ee0816d0904bf862ab0e04c3a9f9ef2
Brandon Dickson <brandondickson@Brandons-Mac-mini.local>
2025-03-02 13:24:09 +0000

    add additional websocket setup and get driver date from API

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+import httpx
 
 app = FastAPI()
 
 
 class ConnectionManager:
     def __init__(self):
-        self.active_connections = list[WebSocket] = []
+        self.active_connections = []
 
     async def connect(self, websocket: WebSocket):
         await websocket.accept()
@@ -14,23 +15,38 @@ class ConnectionManager:
     def disconnect(self, websocket: WebSocket):
         self.active_connections.remove(websocket)
 
-    async def send_personal_message(sale, message: str, websocket: WebSocket):
+    async def send_personal_message(self, message: str, websocket: WebSocket):
         await websocket.send_text(message)
 
     async def broadcast(self, message: str):
         for connection in self.active_connections:
-            await connection.send_text(message)
+            await connection.send_json(message)
 
 
 manager = ConnectionManager()
 
 
+async def getCarData():
+    url = f"https://api.openf1.org/v1/drivers?&session_key=latest"
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        response.raise_for_status()
+
+        data = response.json()
+        return data
+
+
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    await manager.connect()
+    await manager.connect(websocket)
     try:
+        carData = await getCarData()
         while True:
-            await websocket.send_text("Connected")
+            await manager.broadcast(carData)
+
     except WebSocketDisconnect:
+        await manager.send_personal_message("Disconnected", websocket)
         manager.disconnect(websocket)
-        await manager.broadcast("Disconnected")
+
+    except Exception as e:
+        print(f"Error: {e}")
